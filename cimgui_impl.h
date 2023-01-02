666d60234cc2d70e6bd22d23be4c2ecc22fa1b6a
Paul Hindt <phindt@gmail.com>
2023-01-02 01:20:59 -0800

    bulletmind: cimgui backend for d3d11

--- cimgui_impl.h
+++ cimgui_impl.h
@@ -3,7 +3,8 @@
 typedef struct ID3D11Device ID3D11Device;
 typedef struct ID3D11DeviceContext ID3D11DeviceContext;
 struct ID3D11Device;
-struct ID3D11DeviceContext;CIMGUI_API bool ImGui_ImplDX11_Init(ID3D11Device* device,ID3D11DeviceContext* device_context);
+struct ID3D11DeviceContext;
+CIMGUI_API bool ImGui_ImplDX11_Init(ID3D11Device* device,ID3D11DeviceContext* device_context);
 CIMGUI_API void ImGui_ImplDX11_Shutdown(void);
 CIMGUI_API void ImGui_ImplDX11_NewFrame(void);
 CIMGUI_API void ImGui_ImplDX11_RenderDrawData(ImDrawData* draw_data);
--- generator/generator.bat
+++ generator/generator.bat
@@ -10,7 +10,7 @@
 :: impl_definitions.lua for implementation function definitions
 
 :: set your PATH if necessary for LuaJIT or Lua5.1 or luajit with: (for example)
-set PATH=%PATH%;C:\anima;C:\mingws\i686-7.2.0-release-posix-dwarf-rt_v5-rev1\mingw32\bin;
+set PATH=%PATH%;C:\Tools\luapower-all-master\bin\mingw64
 :: set PATH=%PATH%;C:\luaGL;C:\i686-7.2.0-release-posix-dwarf-rt_v5-rev1\mingw32\bin;
 :: set PATH=%PATH%;C:\luaGL\sources\luajit-master\luajit-master\bin\mingw32;C:\mingw32\bin;
 ::process  files
@@ -18,8 +18,7 @@ set PATH=%PATH%;C:\anima;C:\mingws\i686-7.2.0-release-posix-dwarf-rt_v5-rev1\min
 :: arg[2] options as words in one string: internal for imgui_internal generation, freetype for freetype generation, comments for comments generation
 :: examples: "" "internal" "internal freetype comments"
 :: arg[3..n] name of implementations to generate and/or CFLAGS (e.g. -DIMGUI_USER_CONFIG or -DIMGUI_USE_WCHAR32)
-luajit ./generator.lua gcc "internal" glfw opengl3 opengl2 sdl
+luajit ./generator.lua cl "internal" dx11
 
 ::leave console open
 cmd /k
-
